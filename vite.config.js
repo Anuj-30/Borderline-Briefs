fd636696dc2e01861c690d5f253127b3ac9b3c09
Anuj <dk010812@gmail.com>
2025-05-13 14:00:13 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], server: {
+  plugins: [react()],
+  base: "/Borderline-Briefs",
+  server: {
     host: true,  // Allows the server to be accessible from any IP address
     allowedHosts: [
       '329c-2409-40d1-10c5-c55f-74a1-7feb-625e-8dd8.ngrok-free.app', // Add your ngrok URL here
